dbae497532fc7651713fbbc294741d0df18e8be1
Mike <35249588+csgmike@users.noreply.github.com>
2018-02-21 14:17:30 +0800

    add null reference exception, add logger tests

--- lib/Exceptions.ts
+++ lib/Exceptions.ts
@@ -270,3 +270,25 @@ export class KeyAlreadyDefinedException<TKey> extends Exception
         this.name = 'KeyAlreadyDefinedException';
     }
 }
+
+export class NullReferenceException extends Exception
+{
+    constructor(message?: string, innerException?: Exception)
+    {
+        if (innerException)
+        {
+            super(message!, innerException);
+        }
+        else
+        {
+            if (message)
+            {
+                super(message);
+            } else
+            {
+                super();
+            }
+        }
+        this.name = 'NullReferenceException';
+    }
+}
