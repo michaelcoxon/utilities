585603876e389df3335f406788b017f7cfe0a9dc
Mike <35249588+csgmike@users.noreply.github.com>
2018-02-22 09:20:37 +0800

    added usingAsync

--- lib/IDisposable.ts
+++ lib/IDisposable.ts
@@ -1,4 +1,14 @@
-﻿
+﻿/** interface for disposable objects */
+export interface IDisposable
+{
+    dispose(): void;
+}
+
+/**
+ * creates a disposable object then cleans it up after inner has finished execution. DO NOT USE ON PROMISES!!! use `usingAsync` instead.
+ * @param disposableObjectFactory
+ * @param inner
+ */
 export function using<T extends IDisposable, TResult>(disposableObjectFactory: () => T, inner: (disposableObject: T) => TResult): TResult
 {
     const disposableObject = disposableObjectFactory();
@@ -13,8 +23,21 @@ export function using<T extends IDisposable, TResult>(disposableObjectFactory: (
     }
 }
 
-
-export interface IDisposable
+/**
+ * creates a disposable object then cleans it up after inner has resolved.
+ * @param disposableObjectFactory
+ * @param inner
+ */
+export async function usingAsync<T extends IDisposable, TResult>(disposableObjectFactory: () => T, inner: (disposableObject: T) => Promise<TResult>): Promise<TResult>
 {
-    dispose(): void;
+    const disposableObject = disposableObjectFactory();
+
+    try
+    {
+        return await inner(disposableObject);
+    }
+    finally
+    {
+        disposableObject.dispose()
+    }
 }
