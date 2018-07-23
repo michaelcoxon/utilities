21f69178bdfec806494a3cf238379dca4ca16433
Michael Coxon <michaelcoxon@outlook.com.au>
2018-07-23 21:12:45 +0800

    add first char helpers to strings

--- src/Strings.ts
+++ src/Strings.ts
@@ -23,6 +23,16 @@ export namespace Strings
      */
     export const newLine = "\n";
 
+    export function firstCharToLowerCase(str: string): string
+    {
+        return str.charAt(0).toLowerCase() + str.slice(1);
+    }
+
+    export function firstCharToUpperCase(str: string): string
+    {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
     export function format(format: string, ...args: any[]): string
     {
         try
@@ -277,16 +287,97 @@ export namespace Dates
         ],
     };
 
-    const TOKEN_TO_STRING_DELEGATES: { [key: string]: (d: Date, config: DateConfiguration) => string } = {        "d": d => d.getDate().toString(),        "dd": d => Strings.padLeft(d.getDate().toString(), 2, '0'),        "ddd": (d, c) => c.daysOfWeek_Short[d.getDay()],        "dddd": (d, c) => c.daysOfWeek_Long[d.getDay()],        "f": d => Math.round(d.getMilliseconds() / 100).toString(),        "ff": d => Strings.padLeft(Math.round(d.getMilliseconds() / 10).toString(), 2, '0'),        "fff": d => Strings.padLeft(Math.round(d.getMilliseconds()).toString(), 3, '0'),        "F": d =>
-        {            const value = Math.round(d.getMilliseconds() / 100);            return value > 0 ? value.toString() : Strings.empty        },        "FF": d =>
-        {            const value = Math.round(d.getMilliseconds() / 10);            return value > 0 ? Strings.padLeft(value.toString(), 2, '0') : Strings.empty        },        "FFF": d =>
-        {            const value = Math.round(d.getMilliseconds());            return value > 0 ? Strings.padLeft(value.toString(), 3, '0') : Strings.empty        },        "h": d => (d.getHours() % 12 || 12).toString(),        "hh": d => Strings.padLeft((d.getHours() % 12 || 12).toString(), 2, '0'),        "H": d => d.getHours().toString(),        "HH": d => Strings.padLeft(d.getHours().toString(), 2, '0'),        "K": (d, c) => getTimezoneHoursAndMinutesString(d.getTimezoneOffset(), c),        "m": d => d.getMinutes().toString(),        "mm": d => Strings.padLeft(d.getMinutes().toString(), 2, '0'),        "M": d => (d.getMonth() + 1).toString(),        "MM": d => Strings.padLeft((d.getMonth() + 1).toString(), 2, '0'),        "MMM": (d, c) => c.months_Short[d.getMonth()],        "MMMM": (d, c) => c.months_Long[d.getMonth()],        "s": d => d.getSeconds().toString(),        "ss": d => Strings.padLeft(d.getSeconds().toString(), 2, '0'),        "t": (d, c) => (d.getHours() < 12 ? c.am : c.pm)[0],        "tt": (d, c) => (d.getHours() < 12 ? c.am : c.pm),        "y": d => parseInt(d.getFullYear().toString().substr(-2)).toString(),        "yy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-2)).toString(), 2, '0'),        "yyy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-3)).toString(), 3, '0'),        "yyyy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-4)).toString(), 4, '0'),        "yyyyy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-5)).toString(), 5, '0'),        "z": (d, c) => getTimezoneHoursString(d.getTimezoneOffset(), c),        "zz": (d, c) => Strings.padLeft(getTimezoneHoursString(d.getTimezoneOffset(), c), 2, '0'),        "zzz": (d, c) => getTimezoneHoursAndMinutesString(d.getTimezoneOffset(), c),    };    export function getTimezoneHoursAndMinutesString(minutes: number, config: DateConfiguration): string
-    {        let result = Strings.empty;        if (minutes < 0)
-        {            result += "-";        }        else
-        {            result += "+";        }        let dec = minutes / 60;        result += Strings.padLeft(Math.floor(dec).toString(), 2, '0');        result += config.timeSeparator;        result += Strings.padLeft(Math.round((dec - Math.floor(dec)) * 60).toString(), 2, '0');        return result;    }    export function getTimezoneHoursString(minutes: number, config: DateConfiguration): string
-    {        let result = Strings.empty;        if (minutes < 0)
-        {            result += "-";        }        else
-        {            result += "+";        }        let dec = minutes / 60;        result += Math.floor(dec).toString();        return result;    }    export function format(format: string, date: Date): string
+    const TOKEN_TO_STRING_DELEGATES: { [key: string]: (d: Date, config: DateConfiguration) => string } = {
+        "d": d => d.getDate().toString(),
+        "dd": d => Strings.padLeft(d.getDate().toString(), 2, '0'),
+        "ddd": (d, c) => c.daysOfWeek_Short[d.getDay()],
+        "dddd": (d, c) => c.daysOfWeek_Long[d.getDay()],
+        "f": d => Math.round(d.getMilliseconds() / 100).toString(),
+        "ff": d => Strings.padLeft(Math.round(d.getMilliseconds() / 10).toString(), 2, '0'),
+        "fff": d => Strings.padLeft(Math.round(d.getMilliseconds()).toString(), 3, '0'),
+        "F": d =>
+        {
+            const value = Math.round(d.getMilliseconds() / 100);
+            return value > 0 ? value.toString() : Strings.empty
+        },
+        "FF": d =>
+        {
+            const value = Math.round(d.getMilliseconds() / 10);
+            return value > 0 ? Strings.padLeft(value.toString(), 2, '0') : Strings.empty
+        },
+        "FFF": d =>
+        {
+            const value = Math.round(d.getMilliseconds());
+            return value > 0 ? Strings.padLeft(value.toString(), 3, '0') : Strings.empty
+        },
+        "h": d => (d.getHours() % 12 || 12).toString(),
+        "hh": d => Strings.padLeft((d.getHours() % 12 || 12).toString(), 2, '0'),
+        "H": d => d.getHours().toString(),
+        "HH": d => Strings.padLeft(d.getHours().toString(), 2, '0'),
+        "K": (d, c) => getTimezoneHoursAndMinutesString(d.getTimezoneOffset(), c),
+        "m": d => d.getMinutes().toString(),
+        "mm": d => Strings.padLeft(d.getMinutes().toString(), 2, '0'),
+        "M": d => (d.getMonth() + 1).toString(),
+        "MM": d => Strings.padLeft((d.getMonth() + 1).toString(), 2, '0'),
+        "MMM": (d, c) => c.months_Short[d.getMonth()],
+        "MMMM": (d, c) => c.months_Long[d.getMonth()],
+        "s": d => d.getSeconds().toString(),
+        "ss": d => Strings.padLeft(d.getSeconds().toString(), 2, '0'),
+        "t": (d, c) => (d.getHours() < 12 ? c.am : c.pm)[0],
+        "tt": (d, c) => (d.getHours() < 12 ? c.am : c.pm),
+        "y": d => parseInt(d.getFullYear().toString().substr(-2)).toString(),
+        "yy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-2)).toString(), 2, '0'),
+        "yyy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-3)).toString(), 3, '0'),
+        "yyyy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-4)).toString(), 4, '0'),
+        "yyyyy": d => Strings.padLeft(parseInt(d.getFullYear().toString().substr(-5)).toString(), 5, '0'),
+        "z": (d, c) => getTimezoneHoursString(d.getTimezoneOffset(), c),
+        "zz": (d, c) => Strings.padLeft(getTimezoneHoursString(d.getTimezoneOffset(), c), 2, '0'),
+        "zzz": (d, c) => getTimezoneHoursAndMinutesString(d.getTimezoneOffset(), c),
+    };
+
+    export function getTimezoneHoursAndMinutesString(minutes: number, config: DateConfiguration): string
+    {
+        let result = Strings.empty;
+
+        if (minutes < 0)
+        {
+            result += "-";
+        }
+        else
+        {
+            result += "+";
+        }
+
+        let dec = minutes / 60;
+
+        result += Strings.padLeft(Math.floor(dec).toString(), 2, '0');
+        result += config.timeSeparator;
+        result += Strings.padLeft(Math.round((dec - Math.floor(dec)) * 60).toString(), 2, '0');
+
+        return result;
+    }
+
+    export function getTimezoneHoursString(minutes: number, config: DateConfiguration): string
+    {
+        let result = Strings.empty;
+
+        if (minutes < 0)
+        {
+            result += "-";
+        }
+        else
+        {
+            result += "+";
+        }
+
+        let dec = minutes / 60;
+
+        result += Math.floor(dec).toString();
+
+        return result;
+    }
+
+    export function format(format: string, date: Date): string
     {
         const tokens = Object.keys(TOKEN_TO_STRING_DELEGATES);
         tokens.sort((a, b) => b.length - a.length);
@@ -309,4 +400,5 @@ export namespace Dates
         }
 
         return result;
-    }}
+    }
+}
