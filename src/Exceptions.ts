c64bf1c589ffc944c95cf337da710e0155b372de
Michael Coxon <michaelcoxon@outlook.com.au>
2019-02-08 19:32:22 +0800

    fixed a dependency fault

--- src/Exceptions.ts
+++ src/Exceptions.ts
@@ -358,3 +358,25 @@ export class FormatException extends Exception
         this.name = 'FormatException';
     }
 }
+
+export class InvalidOperationException extends Exception
+{
+    constructor(message?: string, innerException?: Exception)
+    {
+        if (innerException)
+        {
+            super(message!, innerException);
+        }
+        else
+        {
+            if (message)
+            {
+                super(message);
+            } else
+            {
+                super();
+            }
+        }
+        this.name = 'InvalidOperationException';
+    }
+}
---
+++ src/Pipeline.ts
@@ -0,0 +1,28 @@
+﻿import { ILogger } from './ILogger';
+import { InvalidOperationException } from './Exceptions';
+
+
+export interface IPipelineTask
+{
+}
+
+
+export class Pipeline<TContext>
+{
+    private readonly _logger: ILogger;
+    private _entryPoint?: IPipelineTask;
+
+    constructor(logger: ILogger)
+    {
+        this._logger = logger;
+    }
+
+    public start<TPipelineTask extends IPipelineTask>(task: TPipelineTask): TPipelineTask
+    {
+        if (this._entryPoint)
+        {
+            throw new InvalidOperationException();
+        }
+        return this._entryPoint = task;
+    }
+}
