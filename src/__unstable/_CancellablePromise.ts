12f25a6789ab2012a1c8f314aa7adc39235e8701
Michael Coxon <michaelccoxon@gmail.com>
2024-12-19 23:02:13 +0800

    is fixed

--- src/__unstable/_CancellablePromise.ts
+++ src/__unstable/_CancellablePromise.ts
@@ -135,3 +135,37 @@ export default class CancellablePromise<T> implements PromiseLike<T>
         return this;
     }
 }
+
+// import assert from 'assert';
+// import CancellablePromise from './_CancellablePromise';
+// import delay from '../Promises/delay';
+
+
+
+// describe("CancellablePromise.construct", () =>
+// {
+//     it("should construct (default)", async () =>
+//     {
+//         let storage = 0;
+
+//         const promise = new CancellablePromise<number>((async () =>
+//         {
+//             await delay(1000);
+//             return storage = 1;
+//         })());
+
+//         promise.cancel();
+
+//         try
+//         {
+//             await delay(1000);
+//         }
+//         catch (e)
+//         {
+//             expect(promise.cancelled).toBe(true);
+//             expect(storage).toBe(0);
+//             return;
+//         }
+//         assert.fail();
+//     });
+// });    
